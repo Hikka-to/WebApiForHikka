61357291a0a2e32c2c7f1d59e16192cf2e7f6118
biguard1 <bogdasha05@gmail.com>
2024-07-11 00:25:05 +0300

    Update GenerationSpec

--- WebApiForHikka.Dtos/DtoTsGenerationSpec.cs
+++ WebApiForHikka.Dtos/DtoTsGenerationSpec.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using TypeGen.Core.SpecGeneration;
+using TypeGen.Core.SpecGeneration.Builders;
 using TypeGen.Core.TypeAnnotations;
 
 namespace WebApiForHikka.Dtos;
@@ -7,60 +8,38 @@ namespace WebApiForHikka.Dtos;
 public class DtoTsGenerationSpec : GenerationSpec
 {
     private const string OutputDir = "./../HikkaTsDtos";
+    private static readonly Assembly Assembly = typeof(DtoTsGenerationSpec).Assembly;
 
     public DtoTsGenerationSpec()
     {
-        var assembly = typeof(DtoTsGenerationSpec).Assembly;
-
         // Class
-        var classTypes = assembly.GetTypes().Where(t =>
-            t.GetCustomAttributes(false).Any(a => a.GetType() == typeof(ExportTsClassAttribute))
-        );
-        foreach (var type in classTypes)
-        {
-            var exportTsClassAttribute = type.GetCustomAttribute<ExportTsClassAttribute>()!;
-            var fullPath = Path.GetFullPath(OutputDir);
-            var namespacePath = "./" + type.Namespace?.Replace("WebApiForHikka.Dtos.", "").Replace(".", "/");
-            var outputDir = Path.Combine(fullPath, namespacePath, exportTsClassAttribute.OutputDir ?? "");
-            outputDir = Path.GetFullPath(outputDir)
-                .Replace(fullPath, OutputDir)
-                .Replace("\\", "/")
-                .TrimEnd('/');
-            AddClass(type, outputDir);
-        }
+        AddTypes<ExportTsClassAttribute>(AddClass);
 
         // Interface
-        var interfaceTypes = assembly.GetTypes().Where(t =>
-            t.GetCustomAttributes(false).Any(a => a.GetType() == typeof(ExportTsInterfaceAttribute))
-        );
-        foreach (var type in interfaceTypes)
-        {
-            var exportTsInterfaceAttribute = type.GetCustomAttribute<ExportTsInterfaceAttribute>()!;
-            var fullPath = Path.GetFullPath(OutputDir);
-            var namespacePath = "./" + type.Namespace?.Replace("WebApiForHikka.Dtos.", "").Replace(".", "/");
-            var outputDir = Path.Combine(fullPath, namespacePath, exportTsInterfaceAttribute.OutputDir ?? "");
-            outputDir = Path.GetFullPath(outputDir)
-                .Replace(fullPath, OutputDir)
-                .Replace("\\", "/")
-                .TrimEnd('/');
-            AddInterface(type, outputDir);
-        }
+        AddTypes<ExportTsInterfaceAttribute>(AddInterface);
 
         // Enum
-        var enumTypes = assembly.GetTypes().Where(t =>
-            t.GetCustomAttributes(false).Any(a => a.GetType() == typeof(ExportTsEnumAttribute))
+        AddTypes<ExportTsEnumAttribute>((t, o) => AddEnum(t, o));
+    }
+
+    private void AddTypes<TAttribute>(Func<Type, string, SpecBuilderBase> method)
+        where TAttribute : Attribute
+    {
+        var types = Assembly.GetTypes().Where(t =>
+            t.GetCustomAttributes(false).Any(a => a.GetType() == typeof(TAttribute))
         );
-        foreach (var type in enumTypes)
+        foreach (var type in types)
         {
-            var exportTsEnumAttribute = type.GetCustomAttribute<ExportTsEnumAttribute>()!;
+            var attribute = type.GetCustomAttribute<TAttribute>()!;
             var fullPath = Path.GetFullPath(OutputDir);
             var namespacePath = "./" + type.Namespace?.Replace("WebApiForHikka.Dtos.", "").Replace(".", "/");
-            var outputDir = Path.Combine(fullPath, namespacePath, exportTsEnumAttribute.OutputDir ?? "");
+            var outputDir = Path.Combine(fullPath, namespacePath,
+                attribute.GetType().Name.Replace("Attribute", "") + "s");
             outputDir = Path.GetFullPath(outputDir)
                 .Replace(fullPath, OutputDir)
                 .Replace("\\", "/")
                 .TrimEnd('/');
-            AddEnum(type, outputDir);
+            method(type, outputDir);
         }
     }
 }
