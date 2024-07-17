15d8b767146048d8a67cb4f13f2ebab1b2511552
PC <PC@DESKTOP-EPJPAG7>
2024-07-17 16:34:56 +0100

    Start working on relateds

---
+++ WebApiForHikka.Application/Relation/Relateds/IRelatedRepository.cs
@@ -0,0 +1,8 @@
+﻿using WebApiForHikka.Application.Shared.Relation;
+using WebApiForHikka.Domain.Models.ManyToMany;
+using WebApiForHikka.Domain.Models.WithoutSeoAddition;
+using WebApiForHikka.Domain.Models.WithSeoAddition;
+
+namespace WebApiForHikka.Application.Relation.AnimeAnimeGroups;
+
+public interface IRelatedRepository : IRelationCrudRepository<Related, Anime, AnimeGroup>;
