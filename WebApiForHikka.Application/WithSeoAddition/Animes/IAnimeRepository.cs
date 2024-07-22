48fb075e8adb4bae1763e54ac1a3f07d4652da28
Black foxi <dentef767@gmail.com>
2024-07-22 17:50:11 +0100

    work on anime and backanime

--- WebApiForHikka.Application/WithSeoAddition/Animes/IAnimeRepository.cs
+++ WebApiForHikka.Application/WithSeoAddition/Animes/IAnimeRepository.cs
@@ -8,4 +8,5 @@ public interface IAnimeRepository : ICrudRepository<Anime>
     public Task<string?> GetPosterPathAsync(Guid animeId);
 
     public string? GetPosterPath(Guid animeId);
+
 }
--- WebApiForHikka.Application/WithoutSeoAddition/AnimeBackdrops/AnimeBackdropService.cs
+++ WebApiForHikka.Application/WithoutSeoAddition/AnimeBackdrops/AnimeBackdropService.cs
@@ -1,14 +1,27 @@
 ﻿using WebApiForHikka.Application.Shared;
 using WebApiForHikka.Domain.Models.WithoutSeoAddition;
+using WebApiForHikka.WebApi.Helper.FileHelper;
 
 namespace WebApiForHikka.Application.WithoutSeoAddition.AnimeBackdrops;
 
-public class AnimeBackdropService(IAnimeBackdropRepository repository)
+public class AnimeBackdropService(IAnimeBackdropRepository repository, IFileHelper fileHelper)
     : CrudService<AnimeBackdrop, IAnimeBackdropRepository>(repository),
         IAnimeBackdropService
 {
+    public IQueryable<AnimeBackdrop> GetAllBackdropForAnime(Guid id)
+    {
+        return _repository.GetAllBackdropForAnime(id);
+    }
+
     public Task<string?> GetImagePathAsync(Guid id)
     {
         return _repository.GetImagePathAsync(id);
+    }
+
+    public override async Task DeleteAsync(Guid id, CancellationToken cancellationToken) 
+    {
+        var backdrop = await _repository.GetAsync(id, cancellationToken);
+
+        fileHelper.DeleteFile(backdrop.Path);
     }
 }
