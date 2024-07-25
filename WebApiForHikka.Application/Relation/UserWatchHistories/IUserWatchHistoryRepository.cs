8f6d41ce0e79ce2cf1053a098176964e9881c985
Black foxi <dentef767@gmail.com>
2024-07-25 15:41:34 +0100

    add UserWatchHistory model full

---
+++ WebApiForHikka.Application/Relation/UserWatchHistories/IUserWatchHistoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using WebApiForHikka.Application.Shared;
+using WebApiForHikka.Application.Shared.Relation;
+using WebApiForHikka.Domain.Models;
+using WebApiForHikka.Domain.Models.WithSeoAddition;
+using WebApiForHikka.SharedModels.Models.Relation;
+
+namespace WebApiForHikka.Application.Relation.UserWatchHistories;
+
+public interface IUserWatchHistoryRepository : IRelationCrudRepository<UserWatchHistory, User, Episode>;
---
+++ WebApiForHikka.WebApiTest/Repository/Relation/UserWatchHistoryRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using WebApiForHikka.Domain.Models;
+using WebApiForHikka.Domain.Models.WithSeoAddition;
+using WebApiForHikka.EfPersistence.Data;
+using WebApiForHikka.EfPersistence.Repositories.Relation;
+using WebApiForHikka.SharedModels.Models.Relation;
+using WebApiForHikka.Test.Shared.Repository;
+
+namespace WebApiForHikka.Test.Repository.Relation;
+
+public class UserWatchHistoryRepositoryTest : SharedRelationRepositoryTest<
+    UserWatchHistory, User, Episode,
+    UserWatchHistoryRepository
+>
+{
+    protected override UserWatchHistory GetSample()
+    {
+        return GetUserWatchHistoryModels.GetSample();
+    }
+
+    protected override UserWatchHistory GetSampleForUpdate()
+    {
+        return GetUserWatchHistoryModels.GetSampleForUpdate();
+    }
+
+    protected override UserWatchHistoryRepository GetRepository(HikkaDbContext hikkaDbContext)
+    {
+        return new UserWatchHistoryRepository(hikkaDbContext);
+    }
+}
