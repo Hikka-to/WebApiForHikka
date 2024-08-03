a13eb849818c336d13039f5359e01ccc7712d3bf
Black foxi <dentef767@gmail.com>
2024-08-03 16:19:00 +0100

    add columns validation in filterPaginationDto

--- WebApiForHikka/Controllers/ControllersWithoutSeoAddition/AnimeBackdropController.cs
+++ WebApiForHikka/Controllers/ControllersWithoutSeoAddition/AnimeBackdropController.cs
@@ -111,6 +111,9 @@ public class AnimeBackdropController(
     {
         var errorEndPoint = ValidateRequest(
             new ThingsToValidateBase());
+        
+        CkeckIfColumnsAreInModel(paginationDto, errorEndPoint);
+
         if (errorEndPoint.IsError) return errorEndPoint.GetError();
 
         var filterPagination = Mapper.Map<FilterPagination>(paginationDto);
